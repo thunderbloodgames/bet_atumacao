a79da22e771cb300206faf5c037203d008cafb2f
thunderbloodgames <thunderbloodgames@gmail.com>
2025-07-31 11:29:46 -0300

    Update get_results.ts

--- api/get_results.ts
+++ api/get_results.ts
@@ -1,5 +1,5 @@
 import type { NextRequest } from 'next/server';
-import { fetchGameResults } from '../services/betsService'; // <--- CORREÇÃO AQUI
+import { fetchGameResults } from '../services/betsService.js'; // <--- CORREÇÃO AQUI
 
 export const runtime = 'edge';
 export async function GET(request: NextRequest) {
